a7834f42a0c25838221ecfeb90b5e39394041b6f
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2024-05-29 15:36:17 +0200

    feat: add legacy image recognizer

--- src/main/java/org/object_d/tensor.java
+++ src/main/java/org/object_d/tensor.java
@@ -11,7 +11,6 @@ public class tensor {
     public static void load_model(String path) {
         try {
             SavedModelBundle model = SavedModelBundle.load(path, "serve");
-            // Now the model is loaded and ready to use
         } catch (Exception e) {
             System.out.println(e);
         }
