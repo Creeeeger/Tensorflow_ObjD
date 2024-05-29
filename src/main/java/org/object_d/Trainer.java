a05f72d9da6e54bd27a5f13ed5f805a95cbada80
Creeeeger <bauaasgd@gmail.com>
2024-05-29 18:04:03 +0200

    add train window

---
+++ src/main/java/org/object_d/Trainer.java
@@ -0,0 +1,13 @@
+package org.object_d;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Trainer extends JFrame {
+
+    public Trainer(JFrame jFrame) {
+        setLayout(new FlowLayout());
+        JLabel label = new JLabel("idk");
+        add(label);
+    }
+}
